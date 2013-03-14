34f52b2099292fedadfbc7ad55d597ace491db8b
Hakan Dilek <hakandilek@gmail.com>
2013-03-14 18:09:07 +0100

    fix path entries

--- copy_local_release.bat
+++ copy_local_release.bat
@@ -1,3 +1,3 @@
 @echo off
-cp -rv %PLAY_HOME%\repository/local/play2-cache releases
-cp -rv %PLAY_HOME%\repository/local/play2-crud releases
+cp -rv '%PLAY_HOME%\repository/local/play2-cache' releases
+cp -rv '%PLAY_HOME%\repository/local/play2-crud' releases
--- copy_local_snapshot.bat
+++ copy_local_snapshot.bat
@@ -1,6 +1,6 @@
 @echo off
-cp -rv %PLAY_HOME%\repository/local/play2-cache snapshots
-cp -rv %PLAY_HOME%\repository/local/play2-crud snapshots
+cp -rv '%PLAY_HOME%\repository/local/play2-cache' snapshots
+cp -rv '%PLAY_HOME%\repository/local/play2-crud' snapshots
 
 mkdir snapshots\com\pickleproject
-cp -Trv %HOME%\.m2\repository\com\pickleproject\ snapshots\com\pickleproject
+cp -Trv '%HOME%\.m2\repository\com\pickleproject\' 'snapshots\com\pickleproject'

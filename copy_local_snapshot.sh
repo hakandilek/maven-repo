7b0d613113245ef329ebd7a9e2a516bd0f124dcf
Hakan Dilek <hakandilek@gmail.com>
2013-03-19 20:15:47 +0100

    update script

--- copy_local_snapshot.sh
+++ copy_local_snapshot.sh
@@ -2,3 +2,5 @@
 cp -rv ${PLAY_HOME}/repository/local/play2-cache snapshots
 cp -rv ${PLAY_HOME}/repository/local/play2-crud snapshots
 
+mkdir snapshots/com/pickleproject
+cp -Trv $HOME/.m2/repository/com/pickleproject/ snapshots/com/pickleproject
